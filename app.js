874f40a232a95cf52912f5147a5dfbc209985dee
Kashif Rahman <95422841+Kashif-rahman03@users.noreply.github.com>
2024-06-09 01:41:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -85,3 +85,95 @@ const checkWinner = () => {
 newGameBtn.addEventListener("click",resetGame);
 resetBtn.addEventListener("click",resetGame);
 drawBtn.addEventListener("click",showDraw);
+
+
+
+
+// let boxes = document.querySelectorAll(".box");
+// let resetBtn = document.querySelector("#reset-btn");
+// let newGameBtn=document.querySelector("#new-btn");
+// let msgContainer=document.querySelector(".msg-container");
+// let msg=document.querySelector("#msg");
+// let turnO = true;
+// let moveCount=0;
+
+// const winPatterns = [
+//   [0, 1, 2],
+//   [0, 3, 6],
+//   [0, 4, 8],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [2, 4, 6],
+//   [3, 4, 5],
+//   [6, 7, 8],
+// ];
+
+// const resetGame=()=>{
+//   turnO=true;
+//   moveCount=0;
+//   enableBoxes();
+//   msgContainer.classList.add("hide");
+// };
+
+// boxes.forEach((box) => {
+//   box.addEventListener("click", () => {
+//     if (box.innerText === "") {
+//       if (turnO) {
+//         box.innerText = "O";
+//         turnO = false;
+//       } else {
+//         box.innerText = "X";
+//         turnO = true;
+//       }
+//       moveCount++;
+//       checkWinner();
+//       box.disabled = true; 
+//     }
+//     if (moveCount === 9) {
+//       showDraw();
+//     }
+//   });
+// });
+
+
+// const disableBoxes=()=>{
+//   for(let box of boxes){
+//     box.disabled=true;
+//   }
+// };
+// const enableBoxes=()=>{
+//   for(let box of boxes){
+//     box.disabled=false;
+//     box.innerText="";
+//   }
+// };
+// const showWinner= (winner) => {
+//   msg.innerText=`Congratulations,Winner is ${winner}`;
+//   msgContainer.classList.remove("hide");
+//   disableBoxes();
+// };
+// const showDraw = () => {
+//   msg.innerText = "It's a Draw!";
+//   msgContainer.classList.remove("hide");
+//   disableBoxes();
+// };
+// const checkWinner = () => {
+//   for (let pattern of winPatterns) {
+    
+//       let pos1Val=boxes[pattern[0]].innerText;
+//       let pos2Val=boxes[pattern[1]].innerText;
+//       let pos3Val=boxes[pattern[2]].innerText;
+
+//       if(pos1Val!=""&&pos2Val!=""&&pos3Val!=""){
+//         if(pos1Val===pos2Val&&pos2Val===pos3Val){
+//           showWinner(pos1Val);
+          
+//         }
+//       }
+//   }
+// };
+
+// newGameBtn.addEventListener("click",resetGame);
+// resetBtn.addEventListener("click",resetGame);
+// drawBtn.addEventListener("click",showDraw);
+
